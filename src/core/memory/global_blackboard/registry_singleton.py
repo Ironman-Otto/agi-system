9bcea5ad8b634338d755392d5ed94a6732d66539
Otto Lecuona <otto@lsdinfotech.com>
2026-04-05 00:39:00 -0400

    created global     blackboard registry singleton and added global blackboard to it

--- src/core/memory/global_blackboard/registry_singleton.py
+++ src/core/memory/global_blackboard/registry_singleton.py
@@ -1,4 +1,4 @@
-from src.core.modules.aem.handler_registry import HandlerRegistry
+from src.core.memory.global_blackboard.handler_registry import HandlerRegistry
 
 # One registry instance for this experiment (singleton-by-module-import)
 registry = HandlerRegistry()
